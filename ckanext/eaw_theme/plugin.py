6887c7ca50a456a141830e085219e1ec04029f56
Christian Foerster <Christian.Foerster@eawag.ch>
2023-02-20 13:22:49 +0000

    reformatted

--- ckanext/eaw_theme/plugin.py
+++ ckanext/eaw_theme/plugin.py
@@ -1,13 +1,15 @@
-import ckan.plugins as plugins
 from collections import OrderedDict
-from ckan.lib.plugins import DefaultTranslation
+
+import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckan.lib.plugins import DefaultTranslation
+
 from ckanext.eaw_core.helpers import (
-    eaw_theme_get_spatial_query_default_extent,
-    eaw_theme_get_default_dataset_type,
-    eaw_theme_patch_activity_actor,
     eaw_helpers_geteawuser,
-    eaw_theme_patch_linked_user
+    eaw_theme_get_default_dataset_type,
+    eaw_theme_get_spatial_query_default_extent,
+    eaw_theme_patch_activity_actor,
+    eaw_theme_patch_linked_user,
 )
 
 
@@ -20,37 +22,43 @@ class EawThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
 
     # IConfigurer
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('assets', 'eaw_theme')
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("assets", "eaw_theme")
 
     # IFacets
     def dataset_facets(self, facet_dict, package_type):
-        new_facets = [('organization', 'Organizations'),
-                      ('groups', 'Projects'),
-                      ('tags', 'Keywords'),
-                      ('variables', 'Variables'),
-                      ('systems', 'Systems'),
-                      ('substances', 'Substances'),
-                      ('taxa', 'Taxa')]
+        new_facets = [
+            ("organization", "Organizations"),
+            ("groups", "Projects"),
+            ("tags", "Keywords"),
+            ("variables", "Variables"),
+            ("systems", "Systems"),
+            ("substances", "Substances"),
+            ("taxa", "Taxa"),
+        ]
         return OrderedDict(new_facets)
 
     def group_facets(self, facet_dict, group_type, package_type):
-        new_facets = [('organization', 'Organizations'),
-                      ('tags', 'Keywords'),
-                      ('variables', 'Variables'),
-                      ('systems', 'Systems'),
-                      ('substances', 'Substances'),
-                      ('taxa', 'Taxa')]
+        new_facets = [
+            ("organization", "Organizations"),
+            ("tags", "Keywords"),
+            ("variables", "Variables"),
+            ("systems", "Systems"),
+            ("substances", "Substances"),
+            ("taxa", "Taxa"),
+        ]
         return OrderedDict(new_facets)
 
     def organization_facets(self, facet_dict, organization_type, package_type):
-        new_facets = [('groups', 'Projects'),
-                      ('tags', 'Keywords'),
-                      ('variables', 'Variables'),
-                      ('systems', 'Systems'),
-                      ('substances', 'Substances'),
-                      ('taxa', 'Taxa')]
+        new_facets = [
+            ("groups", "Projects"),
+            ("tags", "Keywords"),
+            ("variables", "Variables"),
+            ("systems", "Systems"),
+            ("substances", "Substances"),
+            ("taxa", "Taxa"),
+        ]
         return OrderedDict(new_facets)
 
     # ITemplateHelpers
@@ -58,20 +66,17 @@ class EawThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
         # Template helper function names should begin with the name of the
         # extension they belong to, to avoid clashing with functions from
         # other extensions.
-        return {'eaw_theme_get_spatial_query_default_extent':
-                    eaw_theme_get_spatial_query_default_extent,
-                'eaw_theme_get_default_dataset_type':
-                    eaw_theme_get_default_dataset_type,
-                'eaw_theme_patch_activity_actor':
-                    eaw_theme_patch_activity_actor,
-                'eaw_theme_geteawuser':
-                    eaw_helpers_geteawuser,
-                'eaw_theme_patch_linked_user':
-                    eaw_theme_patch_linked_user}
+        return {
+            "eaw_theme_get_spatial_query_default_extent": eaw_theme_get_spatial_query_default_extent,
+            "eaw_theme_get_default_dataset_type": eaw_theme_get_default_dataset_type,
+            "eaw_theme_patch_activity_actor": eaw_theme_patch_activity_actor,
+            "eaw_theme_geteawuser": eaw_helpers_geteawuser,
+            "eaw_theme_patch_linked_user": eaw_theme_patch_linked_user,
+        }
 
     # IPackageController
     def before_search(self, search_params):
-        search_params.update({
-            'sort': search_params.get('sort', 'mydefaultsortparam asc')
-        })
+        search_params.update(
+            {"sort": search_params.get("sort", "mydefaultsortparam asc")}
+        )
         return search_params
