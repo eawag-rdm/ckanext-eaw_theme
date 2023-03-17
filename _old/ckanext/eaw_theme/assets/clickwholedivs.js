0b59698fb0891f09986df76404bd512a20f049be
Christian Foerster <Christian.Foerster@eawag.ch>
2023-03-17 14:50:12 +0000

    removed the _old version of the extension

--- _old/ckanext/eaw_theme/assets/clickwholedivs.js
+++
@@ -1,6 +0,0 @@
-$("li.resource-item").click(function(e) {
-    var url = $(this).children("a.heading").first().attr("href");
-    if ($(e.target).is("li.resource-item")) {
-        window.location = url;
-    }
-});
