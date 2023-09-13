7c49b466acd93c9c1e74e2dd89a3991ababd6519
blagoja <blagojabozinovski@gmail.com>
2023-09-13 13:18:23 +0200

    Modify the test

--- ckanext/eaw_theme/tests/test_plugin.py
+++ ckanext/eaw_theme/tests/test_plugin.py
@@ -49,7 +49,7 @@ To temporary patch the CKAN configuration for the duration of a test you can use
 """
 import pytest
 import ckanext.eaw_theme.plugin as plugin
-from ckan.plugins.core import plugin_loaded
+
 
 
 @pytest.mark.ckan_config("ckan.plugins", "eaw_theme")
