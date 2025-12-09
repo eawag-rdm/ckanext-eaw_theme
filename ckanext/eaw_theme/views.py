2a54502671fd196141d0351a3487c44621023ad9
Christian Förster <christian.foerster@eawag.ch>
2025-12-09 15:41:02 +0100

    name change

--- ckanext/eaw_theme/views.py
+++ ckanext/eaw_theme/views.py
@@ -4,8 +4,8 @@ from ckan.plugins import toolkit
 eaw_theme = Blueprint('eaw_theme', __name__)
 
 
-def help():
+def help_page():
     return toolkit.render('home/help.html')
 
 
-eaw_theme.add_url_rule('/help', view_func=help, endpoint='help')
+eaw_theme.add_url_rule('/help', view_func=help_page, endpoint='help')
